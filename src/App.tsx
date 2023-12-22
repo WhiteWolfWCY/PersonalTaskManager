b855cef096e554814e2b7cfbb3041dbfd2873f74
Mateusz Martyna <mateuszmartyna44@gmail.com>
2023-12-22 22:36:51 +0100

    Creating API connection

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,20 @@ import React, {FC, ReactElement} from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { customTheme } from './theme/customTheme';
 import { Dashboard } from './pages/dashboard/dashboard';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 const App:FC = (): ReactElement => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CssBaseline />
-      <Dashboard />
-    </ThemeProvider> 
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={customTheme}>
+        <CssBaseline />
+        <Dashboard />
+      </ThemeProvider> 
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
